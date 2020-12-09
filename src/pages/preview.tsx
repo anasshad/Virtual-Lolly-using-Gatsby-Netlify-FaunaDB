44b71cc86fe7cc4269cb40c5a9a1c42d6cc3650e
Anas Shad <anasshad@hotmail.com>
2020-12-09 19:57:03 +0500

    Removed preview.tsx error

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -7,32 +7,34 @@ import Lolly from "../components/lolly"
 import "./styles.css"
 
 const Preview = ({ location }) => {
-  console.log(location)
+  if (location) {
+    const { cl1, cl2, cl3, to, from, msg, link } = location.state.data
 
-  const { cl1, cl2, cl3, to, from, msg, link } = location.state.data
+    return (
+      <div className="App">
+        <h2>Virtual Lolly</h2>
+        <div>
+          Your lolly has been created. You can share it by sending this url:
+        </div>
+        <div
+          style={{ border: "1px solid #000", padding: "5px", margin: "1em" }}
+        >{`https://virtual-lolly-anasshad.netlify.app/lolly/${link}`}</div>
+        <div className="main-container">
+          <div className="lolly-container">
+            <Lolly top={cl1} middle={cl2} bottom={cl3} />
+          </div>
+          <div className="message-container">
+            <p className="to">{`Dear ${to},`}</p>
+            <p className="msg">{msg}</p>
+            <p>From, </p>
+            <p className="from">{from}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
-  return (
-    <div className="App">
-      <h2>Virtual Lolly</h2>
-      <div>
-        Your lolly has been created. You can share it by sending this url:
-      </div>
-      <div
-        style={{ border: "1px solid #000", padding: "5px", margin: "1em" }}
-      >{`https://virtual-lolly-anasshad.netlify.app/lolly/${link}`}</div>
-      <div className="main-container">
-        <div className="lolly-container">
-          <Lolly top={cl1} middle={cl2} bottom={cl3} />
-        </div>
-        <div className="message-container">
-          <p className="to">{`Dear ${to},`}</p>
-          <p className="msg">{msg}</p>
-          <p>From, </p>
-          <p className="from">{from}</p>
-        </div>
-      </div>
-    </div>
-  )
+  return <div>Preview</div>
 }
 
 export default Preview
